67bdeab0e26e91bfed43da80ae77501b868a218d
Gavin Vaske <Gavin.Vaske@gmail.com>
2021-03-21 13:42:52 -0500

    feature: send emails when user submits contact us form

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
+require('dotenv').config();
 
 const app = express();
 const defaultPort = 3000;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const {sendEmail} = require('../services/EmailService');
+const STATUS_SUCCESS = 200;
 
 router.get('/', (request, response) => {
     response.render('index');
+});
+
+router.post('/email-contact-form', (request, response) => {
+    let emailInformation = request.body;
+    sendEmail(emailInformation);
+
+    response.status(STATUS_SUCCESS).send();
 });
 
 module.exports = router;
